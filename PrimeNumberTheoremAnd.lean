578f22db7c497233586f4b03ff85b182c48b55c9
Rob Sneiderman <robbysneiderman@gmail.com>
2026-06-16 01:22:26 -0500

    feat(kadiri): export selected PV endpoint

--- PrimeNumberTheoremAnd.lean
+++ PrimeNumberTheoremAnd.lean
@@ -32,6 +32,7 @@ import PrimeNumberTheoremAnd.IEANTN.FKS2
 import PrimeNumberTheoremAnd.IEANTN.FKS2Floor.Cor22Floor
 import PrimeNumberTheoremAnd.IEANTN.Goldbach
 import PrimeNumberTheoremAnd.IEANTN.Kadiri
+import PrimeNumberTheoremAnd.IEANTN.KadiriSelectedPVEndpoint
 import PrimeNumberTheoremAnd.IEANTN.KLN
 import PrimeNumberTheoremAnd.IEANTN.Lcm
 import PrimeNumberTheoremAnd.IEANTN.Li2Bounds
